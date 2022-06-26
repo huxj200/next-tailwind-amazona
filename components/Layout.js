ef05bf5d77f2465ad1beb1086a08502c24055ed3
huxj200 <huxj1441105278@outlook.com>
2022-06-26 16:04:14 +0800

    Video-06-List-Products

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import React from "react";
+import React from 'react';
 
 export default function Layout({ title, children }) {
     return (
