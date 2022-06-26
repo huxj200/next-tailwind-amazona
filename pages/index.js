3ba018fa7bc3088df3872f1cc8b4479523717f35
huxj200 <huxj1441105278@outlook.com>
2022-06-26 18:13:44 +0800

    Create-Product-Details-Page

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,6 @@ import ProductItem from '../components/ProductItem';
 import data from '../utils/data';
 
 export default function Home() {
-  console.log(
-
-    data.products.map(
-      (product) => (
-        <ProductItem product={product} key={product.slug}>
-        </ProductItem>
-      )
-    ),
-
-  );
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
